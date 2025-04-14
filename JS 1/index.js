55024d478c8b57e0c05811086201a2366091c5bd
Aashish Kumar <ashish8803568@gmail.com>
2025-04-14 14:24:36 +0530

    Add files via upload

---
+++ JS 1/index.js
@@ -0,0 +1,73 @@
+// // line below is used to print on the console window
+ console.log('Hello World version4');
+
+// let a=5;
+// console.log(a);
+
+// const num=12;
+// // num=13;
+
+// let lastName='Verma';
+// console.log(lastName);
+
+// lastName= 3;
+// console.log(lastName);
+
+// console.log(5>3);
+
+// let age=17;
+// let status=(age>=18)? 'Yes Vote' : 'No vote' ;
+// console.log(status);
+
+// let marks=78;
+// if(marks>=90) {
+//     console.log('A');
+// }
+// else if (marks>=80) {
+//     console.log('B');
+// }
+
+// else if (marks>=70) {
+//     console.log('C');
+// }
+
+// else if (marks>=60) {
+//     console.log('D');
+// }
+
+// else  {
+//     console.log('E');
+// }
+
+// let num=1;
+// switch(num) {
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     default: console.log('D');
+
+
+// }
+
+
+for(let i=1; i<=5; i=i+1) {
+    console.log("Babbar" +i);
+}
+
+let x=1;
+
+while(x<=5) {
+    console.log(x);
+    x++;
+}
+
+let y=1;
+
+do{
+    console.log(y);
+    y++;
+
+}while(y <10);
